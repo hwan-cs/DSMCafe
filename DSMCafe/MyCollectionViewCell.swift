7cbc40b4d10de25cd19a8d56a2eafa1952a7fd0b
Jung Hwan Park <hwann1005@gmail.com>
2022-10-23 00:40:11 +0900

    init pls

---
+++ DSMCafe/MyCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  MyCollectionViewCell.swift
+//  DSMCafe
+//
+//  Created by Jung Hwan Park on 2022/10/14.
+//
+
+import Foundation
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell
+{
+    @IBOutlet var cellImage: UIImageView!
+    
+    @IBOutlet var cellTitle: UILabel!
+    
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+    }
+}
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'DSMCafe' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for DSMCafe
+  pod 'LTMorphingLabel'
+  pod 'AMPopTip'
+  pod 'Firebase/Firestore'
+  pod 'Firebase/DynamicLinks'
+  pod 'FirebaseStorage'
+end
